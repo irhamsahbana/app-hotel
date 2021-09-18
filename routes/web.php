043ea084fafe35a48ee0a7313ae6bbc07cb2bbca
irhamsahbana <irhamsahbana@gmail.com>
2021-09-18 16:32:39 +0800

    first commit

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\Admin\MenuController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//get
+//post
+//delete
+//put 
+
+Route::delete('/', function () {
+    return view('welcome');
+});
+
+Route::resource('menu', MenuController::class);
+
